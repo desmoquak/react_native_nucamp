c2a3869e05d8829a166ba2018459b24fd47948ea
John Schaub <schaubjs@gmail.com>
2021-04-21 22:13:32 -0600

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -25,6 +25,7 @@ import {
 	fetchPartners,
 } from '../redux/actionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 
 const mapDispatchToProps = {
 	fetchCampsites,
@@ -162,6 +163,31 @@ const ReservationNavigator = createStackNavigator(
 	}
 );
 
+const FavoritesNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Favorites },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+			headerLeft: (
+				<Icon
+					name='heart'
+					type='font-awesome'
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView
@@ -218,6 +244,20 @@ const MainNavigator = createDrawerNavigator(
 				drawerIcon: ({ tintColor }) => (
 					<Icon
 						name='tree'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				),
+			},
+		},
+		Favorites: {
+			screen: FavoritesNavigator,
+			navigationOptions: {
+				drawerLabel: 'My Favorites',
+				drawerIcon: ({ tintColor }) => (
+					<Icon
+						name='heart'
 						type='font-awesome'
 						size={24}
 						color={tintColor}
