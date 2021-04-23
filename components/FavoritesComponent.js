d70008fe855751cfdcf01c76558df2ddb7248e1a
John Schaub <schaubjs@gmail.com>
2021-04-22 22:21:43 -0600

    Animatable

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -9,6 +9,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { deleteFavorite } from '../redux/ActionCreators';
 import { Alert } from 'react-native';
 import { Button } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
 	return {
@@ -93,13 +94,15 @@ class Favorites extends Component {
 			);
 		}
 		return (
-			<FlatList
-				data={this.props.campsites.campsites.filter((campsite) =>
-					this.props.favorites.includes(campsite.id)
-				)}
-				renderItem={renderFavoriteItem}
-				keyExtractor={(item) => item.id.toString()}
-			/>
+			<Animatable.View animation='fadeInRightBig' duration={2000}>
+				<FlatList
+					data={this.props.campsites.campsites.filter((campsite) =>
+						this.props.favorites.includes(campsite.id)
+					)}
+					renderItem={renderFavoriteItem}
+					keyExtractor={(item) => item.id.toString()}
+				/>
+			</Animatable.View>
 		);
 	}
 }
